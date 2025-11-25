389a3b3a38007c983a2ca4a711ccd1265de1abe8
NAVEEN <naveenmattur25@gmail.com>
2025-11-25 20:08:08 +0530

    Move app.py to project root for gunicorn

--- wsgi.py
+++ wsgi.py
@@ -6,4 +6,4 @@ APP_FOLDER = os.path.join(HERE, "Personel-Diary-With-Sentiment-Analysis-main", "
 sys.path.insert(0, APP_FOLDER)
 
 # import the Flask app object from app.py
-from app import app  # assumes your Flask instance is named `app`
+from  import app  # assumes your Flask instance is named `app`
