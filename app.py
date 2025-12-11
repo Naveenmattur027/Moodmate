e932fe1cc4e0bd8926cae741820ccdcc5e70d3ce
Naveenmattur027 <naveenmattur25@gmail.com>
2025-12-11 12:20:48 +0530

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__),
-                                "Personel-Diary-With-Sentiment-Analysis-main",
-                                "Personel-Diary-With-Sentiment-Analysis-main"))
-from app import app
